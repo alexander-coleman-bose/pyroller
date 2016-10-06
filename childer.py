82885cc7f994e7ea0c621259245478bc34f6be3f
Lord Mailman <alexander.g.coleman@gmail.com>
2016-10-06 15:12:36 -0400

    Added rollStats and refactored abilityScores and added new character attributes

--- childer.py
+++ childer.py
@@ -5,11 +5,13 @@ Created on Wed Jun 15 12:00:15 2016
 @author: lordmailman
 """
 
-from math import *
+from math import floor
+from random import randint
 
 class character:
     def __init__(self):
         self.name = []
+        self.portrait = []
 
         self.xp = 0
         self.level = []
@@ -32,6 +34,7 @@ class character:
         self.skin = []
         self.hair = []
         self.appearance = []
+        
         self.personality = []
         self.ideals = []
         self.bonds = []
@@ -53,13 +56,23 @@ class character:
 #        self.cha = self.bCha
 
         self.statBase = {'str':8,'dex':8,'con':8,'int':8,'wis':8,'cha':8}
-        self.statRolled = self.statBase
         self.statRacialBonus = {'str':0,'dex':0,'con':0,'int':0,'wis':0,'cha':0}
         self.stat = self.statBase
         self.statMod = {'str':-1,'dex':-1,'con':-1,'int':-1,'wis':-1,'cha':-1}
 
+        self.pointBuy = True
         self.buyPointsBase = 27
         self.buyPointsRem = 27
+        
+        self.features = []
+        self.attacks = []
+        self.spells = []
+        
+        self.equipment = []
+        self.weapons = []
+        self.armor = []
+        
+        self.coins = []
 
 #        self.strm = int(floor((self.str - 10)/2))
 #        self.dexm = int(floor((self.dex - 10)/2))
@@ -69,16 +82,54 @@ class character:
 #        self.cham = int(floor((self.cha - 10)/2))
 
 #        self.dic = {}
+
+        
+    def rollStats(self,nDice=4,nDrop=1):
+        if nDice<3:
+            raise RuntimeError("You must roll at least 3d6.")
+        elif nDrop<0 or nDrop>=nDice:
+            raise RuntimeError("You must drop a positive number of dice, but no more than you roll.")
+        
+        self.pointBuy = False
+        
+        for stat in self.statBase:
+            rolls = list()
+        
+            for ind in range(nDice):
+                rolls.append(randint(1,6))
+            
+            for ind in range(nDrop):
+                rolls.pop(rolls.index(min(rolls)))
+        
+            self.abilityScore(stat,sum(rolls))
         
 
     def abilityScore(self,ability,newScore = None):
+        acceptedNames = ['str','dex','con','int','wis','cha']
+        ability = str.lower(ability)
+        
+        # interpret full names to stat names
+        if ability == 'strength':
+            ability = 'str'
+        elif ability == 'dexterity':
+            ability = 'dex'
+        elif ability == 'constitution':
+            ability = 'con'
+        elif ability == 'intelligence':
+            ability = 'int'
+        elif ability == 'wisdom':
+            ability = 'wis'
+        elif ability == 'charisma':
+            ability = 'cha'
+        elif ability not in acceptedNames:
+            raise RuntimeError("abilityScore must be called with an ability (str,dex,con,int,wis,cha)")
+        
+        # if no score is given, get the stat instead
         if newScore == None:
             return self.stat[ability]
-        else:
-            adjScore = newScore - self.statRacialBonus[ability]
+        elif self.pointBuy:
+            adjScore = floor(newScore) - self.statRacialBonus[ability]
             cost = 0
-            
-            # if rolled or point buy...
             
             if adjScore < 8:
                 raise RuntimeError("Stat cannot be lowered any further than 8 plus racial bonus.")
@@ -94,17 +145,84 @@ class character:
             else:
                 self.buyPointsRem = self.buyPointsRem - cost
                 
-            self.stat[ability] = newScore
-            self.statMod[ability] = int(floor( (self.stat[ability]-10)/2 ))
+        self.stat[ability] = newScore
+        self.statMod[ability] = floor( (self.stat[ability]-10)/2 )
                 
-        print 'Your stats:'
-        print 'Strength:     {:2d} ({:+2d})'.format(self.str,self.strm)
-        print 'Dexterity:    {:2d} ({:+2d})'.format(self.dex,self.dexm)
-        print 'Constitution: {:2d} ({:+2d})'.format(self.con,self.conm)
-        print 'Intelligence: {:2d} ({:+2d})'.format(self.int,self.intm)
-        print 'Wisdom:       {:2d} ({:+2d})'.format(self.wis,self.wism)
-        print 'Charisma:     {:2d} ({:+2d})'.format(self.cha,self.cham)
-        print 'Remaining buy points: {:3d}'.format(self.buyPointsRem)
+        print('Your stats:')
+        print('Strength:'.ljust(14),
+              repr(self.stat['str']).rjust(3),
+              '({:+d})'.format(self.strm()),sep='')
+        print('Dexterity:'.ljust(14),
+              repr(self.stat['dex']).rjust(3),
+              '({:+d})'.format(self.strm()),sep='')
+        print('Constitution:'.ljust(14),
+              repr(self.stat['con']).rjust(3),
+              '({:+d})'.format(self.strm()),sep='')
+        print('Intelligence:'.ljust(14),
+              repr(self.stat['int']).rjust(3),
+              '({:+d})'.format(self.strm()),sep='')
+        print('Wisdom:'.ljust(14),
+              repr(self.stat['wis']).rjust(3),
+              '({:+d})'.format(self.strm()),sep='')
+        print('Charisma:'.ljust(14),
+              repr(self.stat['cha']).rjust(3),
+              '({:+d})'.format(self.strm()),sep='')
+        print('Remaining buy points:',repr(self.buyPointsRem).rjust(3))
+        
+        
+    def strength(self,newScore = None):
+        if newScore == None:
+            return self.abilityScore('str')
+        else:
+            self.abilityScore('str',newScore)
+            
+    def dexterity(self,newScore = None):
+        if newScore == None:
+            return self.abilityScore('dex')
+        else:
+            self.abilityScore('dex',newScore)
+        
+    def constitution(self,newScore = None):
+        if newScore == None:
+            return self.abilityScore('con')
+        else:
+            self.abilityScore('con',newScore)
+        
+    def intelligence(self,newScore = None):
+        if newScore == None:
+            return self.abilityScore('int')
+        else:
+            self.abilityScore('int',newScore)
+        
+    def wisdom(self,newScore = None):
+        if newScore == None:
+            return self.abilityScore('wis')
+        else:
+            self.abilityScore('wis',newScore)
+        
+    def charisma(self,newScore = None):
+        if newScore == None:
+            return self.abilityScore('cha')
+        else:
+            self.abilityScore('cha',newScore)
+        
+    def strm(self):
+        return self.statMod['str']
+        
+    def dexm(self):
+        return self.statMod['dex']
+        
+    def conm(self):
+        return self.statMod['con']
+        
+    def intm(self):
+        return self.statMod['int']
+        
+    def wism(self):
+        return self.statMod['wis']
+        
+    def cham(self):
+        return self.statMod['cha']
 
 
 #cTest = character()
---
+++ testScript.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 15 12:49:22 2016
+
+@author: lordmailman
+"""
+
+from childer import *
+
+c = character()
