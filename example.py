d8e61690ee01a25ae30afe3a70e635f0049ef445
Alex Coleman <alexander_coleman@bose.com>
2017-01-26 12:55:41 -0500

    updated example, started to create attack example

--- example.py
+++ example.py
@@ -7,22 +7,22 @@ Created on Mon Jan 23 12:07:01 2017
 @author: lordmailman
 """
 
-from roll import roll,rollInfo,rollWin,rollLose,critInfo
+from roll import roll,info,win,lose,crit,compare,attack
 #from roll import *
 
 # roll magic missile damage N*(1d4+1)
 N = 3
 # using roll
 magic_missile = list()
-for ind in range(N): magic_missile.append(roll(1,4,1))
+for ind in range(N): magic_missile.append(roll('1d4+1'))
 
 # using rollInfo to just simulate the roll
 magic_missile = list()
-for ind in range(N): magic_missile.append(rollInfo('1d4+1')['val'])
+for ind in range(N): magic_missile.append(info('1d4+1')['val'])
 
 # using rollInfo to provide the full statistics
 #mmInfo = rollInfo('3d4+3')
-mmInfo = rollInfo(str(N)+'d4+'+str(N))
+mmInfo = info(str(N)+'d4+'+str(N))
 
 print('Firing '+str(N)+' magic missiles, they do...')
 print('\ta minimum of '+str(mmInfo['min'])+' damage.')
@@ -30,7 +30,7 @@ print('\ta maximum of '+str(mmInfo['max'])+' damage.')
 print('\ta mean of '+str(mmInfo['mean'])+' damage.')
 
 # roll an attack roll with advantage and 1d6 Bardic Inspiration (2d20K1 + 1d6 + 5)
-atInfo = rollInfo('2d20k1 + 1d6 + 5')
+atInfo = info('2d20k1 + 1d6 + 5')
 target = 18
 print('\nFor an attack roll with advantage, a +5 modifier, and 1d6 of Bardic Inspiration...')
 print('\tyou will get a minimum result of '+str(atInfo['min'])+'.')
@@ -38,4 +38,21 @@ print('\tyou will get a maximum result of '+str(atInfo['max'])+'.')
 print('\tyou will get a mean result of '+str(atInfo['mean'])+'.')
 print('\tyou have a '+str(atInfo['critHit']*100)+'% chance of critically hitting.')
 print('\tyou have a '+str(atInfo['critMiss']*100)+'% chance of critically missing.')
-print('\tyou have a '+str(rollWin(atInfo,target)*100)+'% chance of hitting an AC of '+str(target)+'.')
+print('\tyou have a '+str(win(atInfo,target)*100)+'% chance of hitting an AC of '+str(target)+'.')
+
+##compare 1-5 level fighter, rogue, wizard, and monk
+# fighter
+level = [1,2,3,4,5]
+prof = [2,2,2,2,3]
+mod = [3,3,3,4,4]
+
+fighter = {}
+fighter['attacks'] = [0]*len(level)
+fighter['damage_die'] = '1d12'
+
+for ind in range(len(level)):
+    fighter['attacks'][ind] = [attack('1d20+'+str(mod[ind]),fighter['damage_die'],str(prof[ind]+mod[ind]))]
+    if level[ind] >= 5:
+        fighter['attacks'][ind].append(attack('1d20+'+str(mod[ind]),fighter['damage_die'],str(prof[ind]+mod[ind])))
+        
+print(fighter)
